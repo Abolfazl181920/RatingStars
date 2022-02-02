8df88fd227be396f9fccbe273e84149a33ab4b43
Abolfazl <abolfazlm181920@gmail.com>
2022-02-02 20:54:37 +0330

    add init codes of components

---
+++ src/Components/Rating.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import RatingStyles from './RatingStyles';
+
+const Rating = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Rating;
---
+++ src/Components/RatingStyles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RatingStyles = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RatingStyles;
